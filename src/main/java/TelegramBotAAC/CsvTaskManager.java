2a63df52522b27ee6874d76a561fe7f7a024cff4
Nehorai0258 <nehorai0258@gmail.com>
2025-06-17 11:37:10 +0300

    Regular

--- src/main/java/TelegramBotAAC/CsvTaskManager.java
+++ src/main/java/TelegramBotAAC/CsvTaskManager.java
@@ -17,6 +17,13 @@ public class CsvTaskManager {
 
 
     public CsvTaskManager() {
+        // ניקוי הקובץ הקיים
+        File fileToDelete = new File(FILE_PATH);
+        if (fileToDelete.exists()) {
+            fileToDelete.delete();
+            System.out.println("הקובץ tasks.csv נמחק בהצלחה");
+        }
+
         File dir = new File(DATA_DIR);
         if (!dir.exists()) {
             dir.mkdirs();
